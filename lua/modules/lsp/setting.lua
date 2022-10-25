ea72eeeb26586e40a36d556aa1f7d16265a1e09f
Uwo-ua <tovstolaki62@gmail>
2022-10-25 10:35:05 +0300

    Support vue

--- lua/modules/lsp/setting.lua
+++ lua/modules/lsp/setting.lua
@@ -1,37 +1,69 @@
 
-
-local present, lspconfig = pcall(require, "lspconfig")
-
-if not present then
+local lsp_status_ok, lspconfig = pcall(require, 'lspconfig')
+if not lsp_status_ok then
   return
 end
 
-local lspconfig_configs = require'lspconfig.configs'
-
-on_attach = function(client, bufnr)
-  client.server_capabilities.documentFormattingProvider = false
-  client.server_capabilities.documentRangeFormattingProvider = false
+local cmp_status_ok, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
+if not cmp_status_ok then
+  return
 end
 
-capabilities = vim.lsp.protocol.make_client_capabilities()
+vim.diagnostic.config({
+  update_in_insert = true,
+  float = {
+    focusable = false,
+    style = "minimal",
+    border = "rounded",
+    source = "always",
+    header = "",
+    prefix = "",
+	},
+})
 
-capabilities.textDocument.completion.completionItem = {
-  documentationFormat = { "markdown", "plaintext" },
-  snippetSupport = true,
-  preselectSupport = true,
-  insertReplaceSupport = true,
-  labelDetailsSupport = true,
-  deprecatedSupport = true,
-  commitCharactersSupport = true,
-  tagSupport = { valueSet = { 1 } },
-  resolveSupport = {
-    properties = {
-      "documentation",
-      "detail",
-      "additionalTextEdits",
-    },
-  },
-}
+-- Show line diagnostics automatically in hover window
+vim.api.nvim_exec([[
+augroup lsp_document_highlight
+  autocmd! * <buffer>
+  autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+  autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+augroup END
+]], false)
+
+vim.api.nvim_command([[packadd cmp-nvim-lsp]])
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+  -- Enable completion triggered by <c-x><c-o>
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  local opts = { noremap = true, silent = true }
+
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+end
+
 
 --[[
   Language servers setup:
@@ -39,6 +71,7 @@ capabilities.textDocument.completion.completionItem = {
   Language server installed:
   Bash          -> bashls
   Lua           -> sumneko_lua
+  Vue           -> vuels
   Python        -> pyright
   C-C++         -> clangd
   HTML/CSS/JSON -> vscode-html-languageserver
@@ -49,27 +82,43 @@ local root_dir = function()
   return vim.fn.getcwd()
 end
 
-local servers = { 'sumneko_lua', 'bashls', 'pyright', 'clangd', 'html', 'cssls', 'tsserver', "vuels", "terraform_lsp" }
+-- Use a loop to conveniently call 'setup' on multiple servers and
+-- Add your language server below:
+local servers = { 'bashls', 'pyright', 'clangd', 'html', 'cssls', 'tsserver', 'sumneko_lua', 'vuels' }
 
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    root_dir = root_dir,
-    capabilities = capabilities,
-    flags = {
-      debounce_text_changes = 150,
-    }
+-- Call setup
+
+for _, server in pairs(servers) do
+  local opts = {
+      on_attach = on_attach,
+      root_dir = root_dir,
+      capabilities = capabilities,
+      flags = {
+        debounce_text_changes = 150,
+      }
   }
+
+  require("lspconfig")[server].setup(opts)
 end
 
-lspconfig.eslint.setup{
+
+require('lspconfig')["vuels"].setup{
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "vue" }
+  settings = {
+    vetur = {
+      validation = {
+        templateProps = true,
+      },
+      completion = {
+        tagCasing = 'initial',
+        autoImport = true,
+        useScaffoldSnippets = true,
+      },
+    },
+  },
 }
 
-lspconfig.volar.setup{
-  filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'}
-}
+
 
 
