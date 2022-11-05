149e853dd88346314f21242bba4ed1aca3226ff4
Uwo-ua <tovstolaki62@gmail>
2022-11-05 18:53:40 +0200

    Up themes

--- lua/modules/theme.lua
+++ lua/modules/theme.lua
@@ -51,3 +51,35 @@ vim.cmd([[
 hi FloatBorder guifg=#f38ba8 guibg=#1e1e2e
 hi NormalFloat guibg=#1e1e2e
 ]])
+
+vim.opt.laststatus = 3
+vim.opt.fillchars:append({
+    horiz = '━',
+    horizup = '┻',
+    horizdown = '┳',
+    vert = '┃',
+    vertleft = '┨',
+    vertright = '┣',
+    verthoriz = '╋',
+})
+
+require('kanagawa').setup({
+    undercurl = true,           -- enable undercurls
+    commentStyle = { italic = true },
+    functionStyle = {},
+    keywordStyle = { italic = true},
+    statementStyle = { bold = true },
+    typeStyle = {},
+    variablebuiltinStyle = { italic = true},
+    specialReturn = true,       -- special highlight for the return keyword
+    specialException = true,    -- special highlight for exception handling keywords
+    transparent = false,        -- do not set background color
+    dimInactive = false,        -- dim inactive window `:h hl-NormalNC`
+    globalStatus = true,       -- adjust window separators highlight for laststatus=3
+    terminalColors = true,      -- define vim.g.terminal_color_{0,17}
+    colors = {},
+    overrides = {},
+    theme = "default"           -- Load "default" theme or the experimental "light" theme
+})
+
+vim.api.nvim_command("colorscheme kanagawa")
