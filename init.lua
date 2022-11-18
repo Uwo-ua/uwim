0e80147f2362a2345e963402a25d7960b7e0b8f4
Uwo-ua <tovstolaki62@gmail>
2022-11-18 09:59:17 +0200

    :add gitsing

--- init.lua
+++ init.lua
@@ -1,21 +1,21 @@
 -- Settings
 require("setting")
-
 -- Plugins
 require("plugin")
-
 -- Key map
 require("mapping")
-
 -- Modules
 require("modules.theme")
 require("modules.telescope")
 require("modules.feline")
 require("modules.winbar")
+require("modules.git")
 require("modules.pairs")
 require("modules.mason")
 require("modules.lsp.init")
+require("modules.lsp.formatter")
 require("modules.cmp")
 require("modules.syntax")
 require("modules.comment")
 require("modules.term")
+require("modules.luasnip")
